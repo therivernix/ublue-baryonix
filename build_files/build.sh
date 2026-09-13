e7bc628df0f6f99fbeba92eb6228483047a16724
therivernix <therivernix@tommygeusens.com>
2026-09-13 11:02:30 +0200

    Adding customizations from custom bluefin 'spino' to custom bazzite 'baryonic'

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,7 @@ cp -avf "/ctx/system_files"/. /
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux
+dnf5 install -y firefox firefox-langpacks yelp
 
 # Use a COPR Example:
 #
@@ -21,6 +21,18 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+
+# Removing built-in extensions
+rm -rf /usr/share/gnome-shell/extensions/burn-my-windows@schneegans.github.com
+rm -rf /usr/share/gnome-shell/extensions/desktop-cube@schneegans.github.com
+rm -rf /usr/share/gnome-shell/extensions/compiz-alike-magic-lamp-effect@hermes83.github.com
+rm -rf /usr/share/gnome-shell/extensions/compiz-windows-effect@hermes83.github.com
+rm -rf /usr/share/gnome-shell/extensions/logomenu@aryan_k
+rm -rf /usr/share/gnome-shell/extensions/restartto@tiagoporsch.github.io
+
+# Compile GSettings schemas for GNOME extensions
+find /usr/share/gnome-shell/extensions -type d -name schemas \
+    -exec glib-compile-schemas {} \;
 
 #### Example for enabling a System Unit File
 
