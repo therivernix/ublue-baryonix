c4103cab66f0a350099143485a33ad24f88495a1
therivernix <therivernix@tommygeusens.com>
2026-09-13 16:28:57 +0200

    Adding explicit gsettings command to enable custom keybindings

--- system_files/usr/share/ublue-os-spinosauros/scripts/gnome/configureKeybindings.sh
+++ system_files/usr/share/ublue-os-spinosauros/scripts/gnome/configureKeybindings.sh
@@ -14,3 +14,21 @@ touch "$SENTINEL"
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < /usr/share/ublue-os-spinosauros/dconf/media-keys.dconf
 dconf load /org/gnome/desktop/wm/keybindings/ < /usr/share/ublue-os-spinosauros/dconf/wm-keybindings.dconf
 dconf load /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ < /usr/share/ublue-os-spinosauros/dconf/custom-keybindings.conf
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom9/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom11/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom12/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom13/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom14/',
+'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom15/'
+]"
